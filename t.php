e4061e9ee57326577edae32d6c489b708643f119
mobotan <mobotan@gmail.com>
2017-11-06 10:06:29 +0000

    Initial commit

--- t.php
+++ t.php
@@ -78,6 +78,7 @@ h2 {
 </style>
 </head>
     
+    
 <body>
 <div class="index-title" id="tablebackground">
     RSM Bingo Game
@@ -90,6 +91,51 @@ h2 {
             <input type="submit" name="p" value="Play" id="submit" />
         </form>
     </div>
+    <div class="celltitle" id="celltitle-load">
+        <h1>Edit</h1>
+        <h2>Edit existing games</h2>
+        <form action="edit.php" method="get" id="load-form">    
+            <input type='text' name='uid' size=6 value='' maxlength="6" placeholder="Game ID" />
+            <input type="submit" name="l" value="Load" id="submit" />
+        </form>
+    </div>
+    <div class="celltitle" id="celltitle-new">
+        <td class="celltitle">      
+            <h1>Cards</h1>
+            <h2>Create a new game
+                <form method="get" action="/edit.php">
+                    <button type="submit" id="submit">New game</button>
+                </form>
+            </h2>
+    </div>
+    <div class="celltitle">
+        <td class="celltitle">  
+            <h1>The best games</h1>    
+            <h2><a href="#">1</a></h2>
+            <h2><a href="#">2</a></h2>
+            <h2><a href="#">3</a></h2>
+            <h2><a href="#">4</a></h2>
+            <h2><a href="#">5</a></h2>
+            
+            <?
+            //require 'vendor/autoload.php';
+            //use Aws\S3\S3Client;
+            //use Aws\S3\Exception\S3Exception;
+            
+            // Instantiate the client.    
+            //$bucket = 'rsm-bingo';
+            //$configfile = 'config.json';
+            //$s3 = new Aws\S3\S3Client(['region'  => 'eu-west-1','version' => 'latest']);
+            
+            //Use the high-level iterators (returns ALL of your objects).
+            //try { $objects = $s3->getIterator('ListObjects', array('Bucket' => $bucket));
+            //    foreach ($objects as $object) {
+            //        echo "<h2>" . $object['Key'] . "\n</h2>";
+            //    }
+            //} catch (S3Exception $e) { echo $e->getMessage() . "\n"; }
+            
+            ?>
+        </div>
     &nbsp;
 </div>
     
