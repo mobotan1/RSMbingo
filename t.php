c9331d35d6372a9eb4d05dffffd038aaac71f41f
mobotan <mobotan@gmail.com>
2017-11-06 10:07:36 +0000

    Initial commit

--- t.php
+++ t.php
@@ -6,6 +6,7 @@
 <title>RSM Bingo Test</title>
 <meta name="description" content="Bingo card game"/>
 <link rel="stylesheet" type="text/css" href="media/css/reset.css" />
+<link rel="stylesheet" type="text/css" href="media/css/bingo.css" />
 
 <style type="text/css">
 body {
@@ -21,60 +22,7 @@ h2 {
     font-family:Din;
     font-size:18px;
 }
-#tablebackground {
-    background: rgb(118,214,255); /* For browsers that do not support gradients */
-    background: -webkit-linear-gradient(rgb(00,84,147), rgb(118,214,255)); /* For Safari 5.1 to 6.0 */
-    background: -o-linear-gradient(rgb(00,84,147), rgb(118,214,255)); /* For Opera 11.1 to 12.0 */
-    background: -moz-linear-gradient(rgb(00,84,147), rgb(118,214,255)); /* For Firefox 3.6 to 15 */
-    background: linear-gradient(rgb(00,84,147), rgb(118,214,255)); /* Standard syntax */
-}
 
-.index-title {
-    margin: auto; 
-    //border-collapse: separate;
-    //border-spacing: 1% 1%;
-    width: 100%;
-    //min-width: 500px;
-    //max-width: 800px;
-    height: 100%;
-    text-align: center;
-    font-family:Avenir;
-    font-size:32px;
-    //padding: 1%;
-    color: white;
-}
-.celltitle{
-    //margin: 1% 3% 1% 3%;
-    margin: 1% auto; 
-    text-align: center;
-    vertical-align: middle;
-    font-family:Avenir;
-    font-size:24px;
-    min-width: 80%;
-    max-width: 90%;
-    //width: 90%;
-    min-height: 5em; //25%;  
-    background-color:#fff; 
-    border:2px solid black;
-    padding:5px;
-    border-collapse:collapse;
-    color: rgb(00,84,147);
-    //border-radius:2px;
-}
-#submit {
-    background-color:rgb(118,214,255);
-    font-size:18px;
-    color:#000;
-    font-family:Avenir;
-    border:none;
-    display:block;
-    margin:auto;
-    padding:5px;
-    margin-top:10px;
-    border-radius:10px;
-    text-align:center;
-    margin-bottom:10px;
-}
 </style>
 </head>
     
